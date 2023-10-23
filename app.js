7104f75aae764a07a975fab1625a654fd5ef5b61
SlavaSid <139711382+SlavaSid@users.noreply.github.com>
2023-10-23 18:57:42 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,44 +1,109 @@
-// 1. Описать функцию-конструктор Shop, которая создает объект магазина. У магазина есть два свойства -  название и адрес. С помощью этого конструктора создать два объекта - например, для магазинов Green и ProStore (можно любые другие).  shops. В итоге должен получиться массив объектов типа:
+// функции-конструкторы
 
-// [{title: 'Green', address:  'ул. Петра Мстиславца 11, Минск'},{title: 'ProStore', address:  'пр-т Дзержинского, 126, Минск'}]
+const array = new Array();
+console.log(array);
 
-// 2. С помощью метода map получить массив, в котором будут содержаться только адреса магазинов. То есть:
+const now = new Date();
+console.log(now);
 
-// ['ул. Петра Мстиславца 11, Минск', 'пр-т Дзержинского, 126, Минск']
+function Auto(color, year){
+// this = {}(неявно)
 
-
-const shops = [];
-function Shop(title, address){
-this.title = title;
-this.address = address;
-shops.push(this);
+this.color = color;
+this.year = year;
+this.stop = function(){
+    console.log(`${this.color} auto stop`);
 }
-const Green = new Shop('Green', 'ул. Петра Мстиславца 11, Минск');
-const ProStore = new Shop('ProStore', 'пр-т Дзержинского, 126, Минск');
-
-const newArr = shops.map(item => item.address)
-console.log(shops);
-console.log(newArr);
+this.move = function(){
+    console.log(`${this.color} auto moves`);
+}
+// return (неявно, работает само)
 
 
-// Задание 2
 
-// Напишите асинхронную функцию, которая получает список пользователей с сервера jsonplaceholder. Выведите список пользователей на страницу. В задании используйте синтаксис async-await и предусмотрите обработку ошибок.
-const url = 'https://jsonplaceholder.typicode.com/users';
-const ul = document.querySelector('.addLi');
-const newFunc = async () => {
-   
-    const response = await fetch(url); 
-    const info = await response.json();
-    info.forEach(element => {
-    ul.innerHTML += `<li>${element.name}</li>`;
-    });
-    };
 
-  
+}
+
+const audi = new Auto('red', 2010);
+console.log(audi);
+audi.move();//red auto moves
+audi.stop();
+
+const tayota = new Auto('gray', 2012);
+console.log(tayota);
+tayota.move();
+
+// try - catch
+
+const newFunc = () => {
+    console.log('hello');
+    //console.log(c);
+    throw new Error('Собственный текст описания ошибки')
+}
+
+
+
+
+
 try{
     newFunc();
 
 } catch(error) {
 console.error(error.message);
 }
+
+// console.log('текст после выполнения функции');
+
+// Задание
+// Используяю fetch отправить запрос на несуществующий урл https://todoappexamplejs.herokuapp.com/no-such-url.json по нажатию на кнопку.
+// Отловить и вывести в консоль ошибку при помощи .catch(err => {})
+
+// fetch(url)
+//   .then()
+//   .then()
+//   .catch((err) => {});
+const url = 'https://jsonplaceholder.typicode.com/todos';
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    fetch(url)
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Статус ответа не тот, который ожидается (не 200)')
+        }
+    
+       return response.json()
+    })
+    .then(json => console.log(json))
+    .catch(error => {
+        console.error(error.message);
+
+    })
+})
+ console.log('текст после выполнения функции');
+
+ // Async Await
+
+
+//  async function myFunction() {
+//     //...
+    
+//  }
+ const myFunct = async () => {
+    //return 'hello'
+    throw new Error('my text');
+ }
+ myFunct().then(info => console.log(info))
+ .catch(error => console.log(error));
+
+
+ const fetchJSON = (url) => {
+     return fetch(url).then(response => response.json())
+ }
+ //fetchJSON(url).then((data) => console.log(data));
+ button.addEventListener('click', async () => {
+    
+    const response = await fetch(url); 
+    const info = await response.json();
+    console.log(info);
+ })
